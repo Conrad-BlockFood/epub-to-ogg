b8030b50f0d2c543b8daa27b52126e7206d57f8a
Kstor <kstor@castorcorp.eu>
2018-03-22 12:33:56 +0100

    zip recursif -r for avoid empty zip xD

--- src/script/script.sh
+++ src/script/script.sh
@@ -17,7 +17,7 @@ ebook-2txt "$TITLE.epub"
 # 2 ogg
 ana-ogg "$TITLE.txt"
 # 2 zip
-zip "$TITLE.zip" "$TITLE.txt_ogg/"
+zip -r "$TITLE.zip" "$TITLE.txt_ogg/"
 
 # return final zip filename
 echo "$TMP_DIR/$TITLE.zip"
